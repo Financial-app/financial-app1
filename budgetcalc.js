9c516c4c4cdcc9fb0e9207e6ba968b4834a420d4
ojurgiel <oliviajurgiel2021@u.northwestern.edu>
2019-11-24 15:44:47 -0600

    graph stuff for budget page

--- budgetcalc.js
+++ budgetcalc.js
@@ -1,7 +1,14 @@
+var sp = 0;
+var sv = 0;
+var incomeg = 0;
+
 function calculatebudget() {
     let spend = Number(document.getElementById("spending").value);
     let save = Number(document.getElementById("savings").value);
     let inc = Number(document.getElementById("income").value);
+    sp = Number(spend);
+    sv = Number(save);
+    incomeg = Number(inc);
 
     let influx = inc - spend;
     if (influx > 0) {
@@ -10,14 +17,50 @@ function calculatebudget() {
         document.getElementById("result").innerHTML="Your savings increase " + influxtext + " dollars each year"
     }
 
-    else if (influx = 0) {
+    else if (influx == 0) {
         document.getElementById("result").innerHTML="Congratulations, your budget is perfectly balanced!"
     }
     else  {
-        let budgetweeks = save - influx;
+        let budgetweeks = save / Math.abs(influx);
         var budgettext = budgetweeks.toString();
         document.getElementById("result").innerHTML = "Your budget will last you " + budgettext + " weeks"
         
     }
+
+
+};
+
+function budgetgraph(){
+    var starting = sv + 4*incomeg - 4*sp
+    var d = [starting, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for (i = 1; i < 12; i++){
+        d[i] = d[i-1] + 4*income - 4*sp
+    }
+
+
+
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "light2",
+        width: 1225,
+        height: 970,
+
+        title:{
+            text: "Your Savings over the Next Year"
+        },
+        axisY:{
+            includeZero: false
+
+        },
+        axisX:{
+            title: "Month"
+        },
+        data: [{
+              		type: "line",
+              		dataPoints: Object.assign({}, d)
+              	}]
+    });
+    chart.render();
 
 };
